a1ff3813a72cf8c3c2765b4392b5ffa5d5f9afcd
Andreas Kihlström <Andreas Kihlstrm>
2016-02-22 16:46:59 +0100

    Server kod

--- ServerTest1/app/src/main/java/MainActivity.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Andreas on 2016-02-22.
- */
-public class MainActivity {
-}
---
+++ ServerTest1/app/src/main/java/com/andreas/servertest1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.andreas.servertest1;
+
+/**
+ * Created by Andreas on 2016-02-22.
+ */
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity
+{
+    Server server;
+    TextView infoip, msg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        infoip = (TextView)findViewById(R.id.infoip);
+        msg = (TextView)findViewById(R.id.msg);
+        server = new Server(this);
+        infoip.setText(server.getIpAdress() + ":" + server.getPort());
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        server.onDestroy();
+    }
+}
