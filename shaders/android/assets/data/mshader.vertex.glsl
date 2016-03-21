a658e6acd6e153f1dee184ba1db872b392b80d29
mlindhe <m.j.lindhe@gmail.com>
2016-03-21 16:57:46 +0100

    Jobbar med att få in vektorer till shader

--- shaders/android/assets/data/mshader.vertex.glsl
+++ shaders/android/assets/data/mshader.vertex.glsl
@@ -9,12 +9,15 @@ uniform mat4 u_projViewTrans;
 
 varying vec2 v_texCoord0;
 varying vec3 v_normal;
-varying vec4 v_light;
+varying vec3 v_light;
+varying vec3 v_eye;
 
 void main(){
     v_texCoord0 = a_texCoord0;
-    v_normal = a_normal;
-    v_light = u_projViewTrans * u_worldTrans * vec4(1.0, 0.0, 0.0, 1.0);
+    v_normal = normalize(a_normal);
+    v_light = normalize(vec3(5.0, 0.0, 0.0) - a_position);
+    vec4 v = u_projViewTrans * vec4(a_position,1.0);
+    v_eye = normalize(vec3(v[0], v[1], v[2]));
 
     gl_Position = u_projViewTrans * u_worldTrans * vec4(a_position, 1);
 }
